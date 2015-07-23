c4f1a310ac02e15c84cc394762308e6f22ebd600
Heidi Roude <HeidiRoude@gmail.com>
2015-07-23 18:01:55 -0400

    created user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 				flash[:success] = "Congratulations!  You have registered.  Please log in."
 			else
 				flash[:failure] = "Registration not successful.   Please try again."
-			end
+			end						## TODO: specific error message / params on email/username
 		redirect_to home_path
 	end
 
@@ -25,7 +25,17 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+			@user = User.find(params[:id])
+	end
 
+	def update
+			@user = User.find(params[:id])
+			@user.update_attributes(user_params)
+	end
+
+	def delete
+	end
 
 	private
 
