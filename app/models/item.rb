4e6d29035e4986c9ba7933fc683061958c03819a
Heidi Roude <HeidiRoude@gmail.com>
2015-08-04 15:55:32 -0400

    before_create add http:// to url link

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,17 @@
 class Item < ActiveRecord::Base
 
+	before_create :format_url
+
 	validates_presence_of :user_id, :name
 
 	belongs_to :user
+
+	def format_url		
+		if url_link.start_with?("http://")
+			url_link
+		else
+			url_link.prepend("http://") 
+		end
+	end
+
 end
