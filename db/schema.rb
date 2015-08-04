4e9071d74ed15908b81f1890a0b8b9df4d555cdd
Heidi Roude <HeidiRoude@gmail.com>
2015-08-04 14:55:53 -0400

    added categories radio button

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150731151403) do
 
   create_table "users", force: :cascade do |t|
     t.string   "password_digest"
-    t.string   "email"
+    t.string   "e clmail"
     t.string   "name"
     t.date     "birthday"
     t.string   "current_interests"
